63b6a0d20f49827deb7fa3cbce89048e0ad06e53
Guillem <1421253@campus.euss.org>
2020-04-11 13:28:25 +0200

    Added doxygen comments.

--- robotps4.py
+++ robotps4.py
@@ -8,34 +8,62 @@ import logging
 import time
 import argparse
 
+## Aquest clase encapsula el controllador deel robot a travers d'un comanament de la PS4.
+#
+#  - El controlador del comanament de la PS4 es fa a travers del paquet de Python @b pygame
+#  que permet obtenir els events de moviment i botons del mateix.
+#  
+#  - El client de MQQT que permet enviar les comanes cap al robot es fa a travers del paquet de Python @b paho
+#    que permet publicar events de MQTT tant a brokers locals com remots.
+#
 class RobotPS4Controller(object):
-    """Class representing the Robot controller."""
 
-    controller      = None
-    axis_data       = None
-    button_data     = None
-    hat_data        = None
+    ##  el boto amb una creu del comanament de la PS4.
+    #
     CROSS_BUTTON    = 0
-    CIRCLE_BUTTON   = 1
-    TRIANGLE_BUTTON = 2
-    SQUARE_BUTTON   = 3
-    L1_BUTTON       = 4
-    R1_BUTTON       = 5
-    L2_BUTTON       = 6
-    R2_BUTTON       = 7
-    SHARE_BUTTON    = 8
-    OPTIONS_BUTTON    = 9
-    HAT_UP = (0,1)
-    HAT_DOWN = (0,-1)
-    HAT_RIGHT = (1,0)
-    HAT_LEFT =(-1,0)
     
+    ##  el boto amb un cercle del comanament de la PS4.
+    #
+    CIRCLE_BUTTON   = 1
+    
+    ##  el boto amb un triangle del comanament de la PS4.
+    #
+    TRIANGLE_BUTTON = 2
+    
+    ##  el boto amb un cuadrat del comanament de la PS4.
+    #
+    SQUARE_BUTTON   = 3
+
+    ##  el boto L1 del comanament de la PS4.
+    #
+    L1_BUTTON       = 4
+    
+    ##  el boto R1 del comanament de la PS4.
+    #
+    R1_BUTTON       = 5
+    
+    ##  el boto L2 del comanament de la PS4.
+    #
+    L2_BUTTON       = 6
+    
+    ##  el boto R2 del comanament de la PS4.
+    #
+    R2_BUTTON       = 7
+
+    ##  el boto SHARE del comanament de la PS4.
+    #
+    SHARE_BUTTON    = 8
+    
+    ##  el boto OPTIONS del comanament de la PS4.
+    #
+    OPTIONS_BUTTON    = 9
+    
+    ## Instancia el modul de @b pygame i espera a que hi hagi un joystick conectat.
+    #
     def init(self):
-        """Initialize the joystick components"""
-        
         pygame.init()
 
-        # initialize the joysitck and wait for one connected.
+        # esperem que hi hagi un joystick conectat.
         #
         pygame.joystick.init()
         if (pygame.joystick.get_count() == 0):
@@ -46,49 +74,53 @@ class RobotPS4Controller(object):
                 pygame.joystick.init()
                 joyready = (pygame.joystick.get_count() != 0)
             
-        self.controller = pygame.joystick.Joystick(0)
-        self.controller.init()
+        ## instancia i inicialitza el controlador de la PS4.
+        #
+        self._controller = pygame.joystick.Joystick(0)
+        self._controller.init()
 
+    ## transforma la coordenada del joystick a la posicio del motor en el rang [0.0, 1.0]
+    #  @param[in] value la coordenada el joystick.
+    #  @return la posicio del motor en el rang [0.0, 1.0]
+    #
     def value2pos (self, value):
         position = (value + 1.0) / 2.0
         return min(max(position, 0.0),1.0)
         
+    ## comença a escoltar events (moviments del joystick i pulsacions del botons)
+    #  i els converteix a comanes de MQTT cap al robot.
+    #  @note aquesta funcio es quedara bloquejada fins que es pitji el boto amb la creu.
+    #
     def listen(self):
-        """Listen for events to happen"""
         
-        if not self.axis_data:
-            self.axis_data = {}
-
-        if not self.button_data:
-            self.button_data = {}
-            for i in range(self.controller.get_numbuttons()):
-                self.button_data[i] = False
-
-        if not self.hat_data:
-            self.hat_data = {}
-            for i in range(self.controller.get_numhats()):
-                self.hat_data[i] = (0, 0)
-
-        quit        = False
-        left_arm    = False
-        left_arm_p  = -1.0
-        right_arm   = False
-        right_arm_p = -1.0
-        neckLR      = False
-        neckLR_p    = -1.0
-        left_eye    = False
-        left_eye_p  = -1.0
-        right_eye   = False
-        right_eye_p = -1.0
-        neck_Body_UD = False
+        quit           = False
+        left_arm       = False
+        left_arm_p     = -1.0
+        right_arm      = False
+        right_arm_p    = -1.0
+        neckLR         = False
+        neckLR_p       = -1.0
+        left_eye       = False
+        left_eye_p     = -1.0
+        right_eye      = False
+        right_eye_p    = -1.0
+        neck_Body_UD   = False
         neck_Body_UD_p = -1
-        neck_UD = False
-        neck_UD_p = -1
-        actions = False
-        state   = 0
+        neck_UD        = False
+        neck_UD_p      = -1
+        actions        = False
+        state          = 0
         
+        # processem els events fins que no s'indiqui el final.
+        #
         while not quit:
+            
+            # llegim un event (boto o moviments del joystick).
+            #
             for event in pygame.event.get():
+
+                # moviment del joystick.
+                #
                 if event.type == pygame.JOYAXISMOTION:
                     if event.axis == 0:
                         if neckLR:
@@ -168,8 +200,8 @@ class RobotPS4Controller(object):
                             if position != neck_Body_UD_p:
                                 publish.single ("robot/body/neck/move", payload=str(position) , hostname='localhost')
                                 
-
-                                
+                # boto cap avall.
+                #
                 elif event.type == pygame.JOYBUTTONDOWN:
                     logging.debug("down {} button".format(event.button))
                     if event.button == self.L2_BUTTON:
@@ -189,6 +221,8 @@ class RobotPS4Controller(object):
                     elif event.button == self.OPTIONS_BUTTON:
                         actions = True
                         
+                # boto cap amunt.
+                #
                 elif event.type == pygame.JOYBUTTONUP:
                     logging.debug("up {} button".format(event.button))
                     if event.button == self.CROSS_BUTTON:
